f5ddf208b5866f582d925deae621322cddf1c332
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-11-28 20:40:52 +0700

    make Post entity, add id generated value and make column slug unique

--- src/main/java/com/devano/blog_app/entity/Post.java
+++ src/main/java/com/devano/blog_app/entity/Post.java
@@ -1,15 +1,20 @@
 package com.devano.blog_app.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String body;
+    @Column(unique = true)
     private String slug;
     private boolean isPublished;
     private boolean isDeleted;
-    private Integer createdAt;
+    private Long createdAt;
     private Integer publishedAt;
 }
--- src/main/java/com/devano/blog_app/service/PostService.java
+++ src/main/java/com/devano/blog_app/service/PostService.java
@@ -1,7 +1,58 @@
 package com.devano.blog_app.service;
 
+import com.devano.blog_app.entity.Post;
+import com.devano.blog_app.repository.PostRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class PostService {
+
+    PostRepository postRepository;
+
+    public Iterable<Post> getPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostBySlug(String slug) {
+        return postRepository.findFirstBySlug(slug).orElse(null);
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Post updatePostById(Integer postId, Post sentPostByUser) {
+        Optional<Post> retreivedPost = postRepository.findById(postId);
+        if(retreivedPost.isPresent()) {
+            sentPostByUser.setSlug(sentPostByUser.getSlug());
+            sentPostByUser.setTitle(sentPostByUser.getTitle());
+            return retreivedPost.get();
+        }
+        return null;
+    }
+
+    public Boolean deletePostById(Integer id) {
+        Post savedPost = postRepository.findById(id).orElse(null);
+        if(savedPost == null) {
+            return false;
+        }
+        savedPost.setDeleted(true);
+        postRepository.save(savedPost);
+        return true;
+    }
+
+    public Boolean publishPost(Integer id) {
+        Optional<Post> savedPostById = postRepository.findById(id);
+        if(savedPostById.isPresent()) {
+            Post post = savedPostById.get();
+            post.setPublished(true);
+            postRepository.save(post);
+            return true;
+        }
+        return false;
+    }
 }
