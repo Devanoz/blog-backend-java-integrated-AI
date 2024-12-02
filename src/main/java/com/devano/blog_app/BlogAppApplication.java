ea6fda6e23a219cc81a7e37f5374bb8f007f7fd1
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-03 06:02:13 +0700

    enable configuration properties for secret.properties

--- src/main/java/com/devano/blog_app/BlogAppApplication.java
+++ src/main/java/com/devano/blog_app/BlogAppApplication.java
@@ -1,9 +1,12 @@
 package com.devano.blog_app;
 
+import com.devano.blog_app.properties.SecretProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({SecretProperties.class})
 public class BlogAppApplication {
 
 
