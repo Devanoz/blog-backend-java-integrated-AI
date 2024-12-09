351a97eb91527cc02301acd8b82a31650b5b6be9
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-09 11:09:12 +0700

    add category controller

---
+++ src/main/java/com/devano/blog_app/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.devano.blog_app.controller;
+
+import com.devano.blog_app.request.category.CreateCategoryRequest;
+import com.devano.blog_app.response.category.CreateCategoryResponse;
+import com.devano.blog_app.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@AllArgsConstructor
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CreateCategoryResponse> createCategory(@RequestBody CreateCategoryRequest categoryRequest) {
+        categoryService.createCategory(categoryRequest);
+        return null;
+    }
+}
