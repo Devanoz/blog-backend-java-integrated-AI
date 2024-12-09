dea6e5624584c3762a24ac5b1195cc39ea68caa1
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-09 11:00:04 +0700

    add response for login

---
+++ src/main/java/com/devano/blog_app/response/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.devano.blog_app.response.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
