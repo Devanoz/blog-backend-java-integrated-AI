0346d4cc7748b05bcdd636b9954b4d8596cdbd67
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-09 11:00:36 +0700

    add JwtAuthenticationFilter

---
+++ src/main/java/com/devano/blog_app/config/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.devano.blog_app.config;
+
+import com.devano.blog_app.service.JwtService;
+import com.devano.blog_app.service.UserDetailService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private UserDetailService userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String jwt = authHeader.substring("Bearer ".length());
+        String username;
+        try{
+           username  = jwtService.getUsername(jwt);
+            if(username!=null) {
+                UserDetails userDetails = userDetailService.loadUserByUsername(username);
+                if(userDetails !=null) {
+                    UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                            userDetails.getUsername(),
+                            userDetails.getPassword(),
+                            userDetails.getAuthorities()
+                    );
+                    token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(token);
+                }
+            }
+        }catch (JwtException e) {
+            // Send 401 Unauthorized response
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json");
+            try (PrintWriter writer = response.getWriter()) {
+                writer.write("{\"error\": \""+ e.getMessage() +"\"}");
+            }
+            return; // Stop further processing of the filter chain
+        }
+        filterChain.doFilter(request,response);
+    }
+}
