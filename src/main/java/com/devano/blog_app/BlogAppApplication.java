e30ca4ccd0fc6950741d10529785d48291e2cb5f
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-09 11:00:15 +0700

    enable caching in project

--- src/main/java/com/devano/blog_app/BlogAppApplication.java
+++ src/main/java/com/devano/blog_app/BlogAppApplication.java
@@ -4,8 +4,10 @@ import com.devano.blog_app.properties.SecretProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({SecretProperties.class})
 public class BlogAppApplication {
 
