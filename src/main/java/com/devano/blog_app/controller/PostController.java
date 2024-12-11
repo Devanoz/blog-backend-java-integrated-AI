7924cdc56c6e66257a97958753ac2ef0c5fc514e
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-11 09:33:47 +0700

    add post controller

--- src/main/java/com/devano/blog_app/controller/PostController.java
+++ src/main/java/com/devano/blog_app/controller/PostController.java
@@ -1,19 +1,59 @@
 package com.devano.blog_app.controller;
 
 import com.devano.blog_app.entity.Post;
+import com.devano.blog_app.request.post.CreateGeminiPostRequest;
+import com.devano.blog_app.request.post.CreatePostRequest;
+import com.devano.blog_app.response.post.CreatePostResponse;
 import com.devano.blog_app.service.PostService;
+import io.github.bucket4j.Bucket;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/posts")
 @AllArgsConstructor
 public class PostController {
     PostService postService;
 
-    public List<Post> getPosts() {
-        return List.of(new Post());
+    @GetMapping
+    public List<CreatePostResponse> getPosts() {
+        return postService.getPosts();
     }
+
+    @PostMapping
+    public Post createPost(@RequestBody @Valid CreatePostRequest post) {
+        return postService.createPost(post);
+    }
+
+    @PostMapping("/ai/gemini")
+    public ResponseEntity<CreatePostResponse> createPost(@RequestBody @Valid CreateGeminiPostRequest post) {
+        var response = postService.createPostUsingGemini(post);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/{slug}")
+    public Post getPostBySlug(@PathVariable("slug") String slug) {
+        return postService.getPostBySlug(slug);
+    }
+
+    @PutMapping("/{id}")
+    public Post editPostById(@PathVariable("id") Integer postId, @RequestBody Post sentPostByUser) {
+        return postService.updatePostById(postId,sentPostByUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deletePostBySlug(@PathVariable Integer id) {
+        return postService.deletePostById(id);
+    }
+
+    @PostMapping("/{id}/publish")
+    public Boolean publishPost(@PathVariable("id")Integer postId) {
+        return postService.publishPost(postId);
+    }
+
 }
